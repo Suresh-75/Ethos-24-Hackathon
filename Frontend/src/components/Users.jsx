67e0a373d36478bc251acee6abd508da7d62ac8f
suresh Kumar <suresh710k@gmail.com>
2024-09-21 19:55:37 +0530

    UI

---
+++ Frontend/src/components/Users.jsx
@@ -0,0 +1,13 @@
+function Users() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className="border-b-[1px] w-[90%] text-center border-purple-200 bold text-2xl py-3 tracking-wide font- text-white">
+          ONLINE-USERS
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Users;
